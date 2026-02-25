f228ab6551896f1088beccbf16a59423c5b69ca0
NojPle <nojple@ktu.lt>
2026-02-25 18:46:21 +0200

    NewGameBut

--- main_menu.gd
+++ main_menu.gd
@@ -13,3 +13,7 @@ func _process(delta: float) -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_new_game_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Game.tscn")
