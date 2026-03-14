10801c9a66726ccda86ac6d21f42cbba7c0f3f41
NojPle <nojple@ktu.lt>
2026-03-14 14:57:53 +0200

    Coin png+ movement (#20)
    Coin png+ movement

--- coins_label.gd
+++ coins_label.gd
@@ -1,6 +1,7 @@
 extends Label
 
 var displayed_amount := 0
+@onready var coin = $"../Coin"  # TextureRect node
 
 func _ready():
 	displayed_amount = Currency.coins
@@ -8,11 +9,20 @@ func _ready():
 	Currency.coins_changed.connect(animate_to)
 
 func animate_to(new_amount):
+	var animation_duration := 0.25  # tiek laiko truks viskas
 	var tween = create_tween()
-	tween.tween_property(self, "scale", Vector2(1.15, 1.15), 0.08)
-	tween.tween_property(self, "scale", Vector2(1, 1), 0.08)
-	tween.parallel().tween_method(update_text, displayed_amount, new_amount, 0.25)
+
+	# Visi tweenai parallel, prasideda tuo pačiu metu
+	tween.parallel().tween_property(coin, "scale", Vector2(1.3, 1.3), animation_duration / 2)
+	tween.parallel().tween_property(coin, "scale", Vector2(1, 1), animation_duration / 2).set_delay(animation_duration / 2)
+
+	tween.parallel().tween_property(self, "scale", Vector2(1.15, 1.15), animation_duration / 2)
+	tween.parallel().tween_property(self, "scale", Vector2(1, 1), animation_duration / 2).set_delay(animation_duration / 2)
+
+	# Skaičiaus animacija vyksta tuo pačiu metu
+	tween.parallel().tween_method(update_text, displayed_amount, new_amount, animation_duration)
+
 	displayed_amount = new_amount
 
 func update_text(amount):
-	text = "🪙 " + str(int(amount))
+	text = str(int(amount))
