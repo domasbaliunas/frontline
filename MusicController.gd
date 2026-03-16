97580db1b87ffc41386cc6d5fa60033456589174
Tautvydas Tiškus <tautvydastiskus@gmail.com>
2026-03-16 11:38:58 +0200

    Created menu options tab that allows to change volume settings (#27)

---
+++ MusicController.gd
@@ -0,0 +1,22 @@
+extends AudioStreamPlayer  
+
+var music_volume: float = 0.5
+var muted: bool = false
+
+func _ready():
+	pass
+
+func apply_settings():
+	if muted:
+		self.volume_db = -80
+	else:
+		self.volume_db = linear_to_db(music_volume)
+		print("Tikrasis grotuvo volume_db: ", self.volume_db)
+	
+func toggle_mute(is_muted: bool):
+	muted = is_muted
+	apply_settings()
+
+func set_volume(value: float):
+	music_volume = value
+	apply_settings()
--- main_menu.gd
+++ main_menu.gd
@@ -17,3 +17,6 @@ func _on_quit_button_pressed() -> void:
 
 func _on_new_game_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Game.tscn")
+	
+func _on_options_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://OptionsMenu.tscn")
