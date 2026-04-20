6ec9c69c8d1e04eb3898cb7668a5bd0a6290a23d
Tautvydas Tiškus <tautvydastiskus@gmail.com>
2026-04-20 15:57:58 +0300

    SCRUM-57 damage flash implementation (#53)
    * damage flash implementation
    
    * print removed

--- scripts/core/player.gd
+++ scripts/core/player.gd
@@ -3,6 +3,7 @@ extends Area2D
 
 @export var hp := 10
 @onready var hp_label = $"../HBoxContainer2/HP"
+@onready var flash_layer = $"../CanvasLayer"
 
 func _ready():
 	body_entered.connect(_on_body_entered)
@@ -12,6 +13,10 @@ func _on_body_entered(body):
 	if body.is_in_group("mobs"):
 		hp -= 1
 		body.queue_free()
+		
+		if flash_layer:
+			flash_layer.play_damage_flash()
+			
 		update_ui()
 
 		if hp <= 0:
