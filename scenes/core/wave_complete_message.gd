75910989a0de7303182514bce2372218443f921f
Tautvydas Tiškus <tautvydastiskus@gmail.com>
2026-04-28 19:46:37 +0300

    SCRUM-49 wave completed implementation (#62)
    * wave completed implementation
    
    * Updated wave completed

---
+++ scenes/core/wave_complete_message.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+@onready var completedLabel: Label = $CompletedLabel
+
+func _ready():
+	visible = false
+	process_mode = Node.PROCESS_MODE_WHEN_PAUSED
+
+func show_message():
+	visible = true
+	await get_tree().create_timer(2.0, true, false, true).timeout
+	visible = false
